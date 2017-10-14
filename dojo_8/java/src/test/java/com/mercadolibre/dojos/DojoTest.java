3984e22c485e29cb437179cb68ce7a635719a37a
Nelson Puglisi <nelson.puglisi@mercadolibre.com>
2017-10-14 11:19:55 -0300

    gson & mockito

--- dojo_8/java/src/test/java/com/mercadolibre/dojos/DojoTest.java
+++ dojo_8/java/src/test/java/com/mercadolibre/dojos/DojoTest.java
@@ -1,13 +1,34 @@
 package com.mercadolibre.dojos;
 
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.stream.JsonReader;
+import com.mercadolibre.dojos.dto.CheckoutOptionsDto;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import static org.mockito.Mockito.mock;
 
 /**
  * Tests for the dojo.
  */
 public class DojoTest {
 
+    @Test
+    public void test() throws FileNotFoundException {
+        CheckoutOptionsDto checkoutOptionsDto = new GsonBuilder().create().fromJson(loadFile("options.json"), CheckoutOptionsDto.class);
 
-	
+        Assert.assertEquals("MLA0000001", checkoutOptionsDto.getItem().getId());
+    }
+
+    private InputStreamReader loadFile(String filename) {
+        return new InputStreamReader(Thread.currentThread().getContextClassLoader().getResourceAsStream(filename));
+    }
+
 }
